3f62acc9838aa2dfec430d79f760618fe76573d0
danielbarreto1 <danielbarreeto@gmail.com>
2022-05-03 22:24:37 -0300

    q5

---
+++ q5.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+
+> ou 1> 	redireciona para saída
+>> 	redireciona para fim de arquivo
+< 	redireciona para a entrada
+<< 	redireciona para a entrada e mantém a entrada aberta até que seja digitado algum caractere de EOF (fim de arquivo) como, por exemplo, CTRL+D
+>& ou 2> 	redireciona a saída de erros
+| 	redireciona a saída de um comando para a entrada de um outro comando
